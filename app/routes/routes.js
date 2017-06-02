53de943f522b8897aa2a7a9bd0b2164fc66f561d
ch1bi <maine1191@gmail.com>
2017-06-01 20:55:18 -0400

    added blogPosts and etc

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,1 +1,12 @@
-var routes = require("./routes.json");
+var r = require("./routes.json");
+
+var controllers = {
+
+pages: require("./controllers/pages")
+}
+
+module.exports = function(app){
+
+    app.get(r.index, controllers.pages.index);
+
+};
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bcrypt = require("bcrypt-nodejs");
 var hbs = require("hbs");
 var path = require("path");
 var session = require("express-session");
+var routes = require("./app/routes/routes");
 
 var app = express();
 
@@ -12,7 +13,15 @@ var app = express();
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "app/views"));
 
+routes(app);
 app.use("/static", path.join(__dirname, "app/client"));
+
+app.use(session({
+
+    secret:"itsASecret",
+    resave:true,
+    saveUninitialized:true
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -21,4 +30,7 @@ app.use(bodyParser.urlencoded({
     
 }));
 
+app.listen(function(){
 
+    console.log("Listening on port 8080");
+});
